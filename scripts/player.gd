5fb7edd0c0a63b7289c35fa2bfb17915ab7a183e
Chesca <fdjr401@gmail.com>
2025-02-27 10:47:38 +0800

    Add comment header and remove unneeded comments

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,25 +1,33 @@
+# I hereby attest to the truth of the following facts:
+#
+# I have not discussed the GDScript code in my program with anyone
+# other than my instructor or the teaching assistants assigned to this course.
+# 
+# I have not used GDScript code obtained from another student, or
+# any other unauthorized source, whether modified or unmodified.
+#
+# If any GDScript code or documentation used in my program was
+# obtained from another source, it has been clearly noted with citations in the
+# comments of my program.
+
 extends CharacterBody2D
 
 const SPEED = 150.0
 const JUMP_VELOCITY = -320.0
 
-var isClimbing : bool = false
+var isClimbing = false
 var XDirection
 var YDirection
 
 @onready var animated_sprite = $AnimatedSprite2D
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
 	if not is_on_floor() and not isClimbing:
 		velocity += get_gravity() * delta
 
-	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor() and not isClimbing:
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	if isClimbing:
 		XDirection = Input.get_axis("move_left", "move_right")
 		YDirection = Input.get_axis("move_up", "move_down")
@@ -31,20 +39,17 @@ func _physics_process(delta: float) -> void:
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 	
-	# velcoity.y should only be modified this way if climbing
 	if isClimbing:
 		if YDirection:  
 			velocity.y = YDirection * SPEED
 		else:
 			velocity.y = move_toward(velocity.x, 0, SPEED)
 			
-	# Flip the sprite depending on direction
 	if XDirection > 0:
-		animated_sprite.flip_h = false  # Moving right
+		animated_sprite.flip_h = false
 	elif XDirection < 0:
-		animated_sprite.flip_h = true   # Moving left
+		animated_sprite.flip_h = true
 
-	#Play animations
 	if isClimbing:
 		animated_sprite.play("climb")
 	elif not is_on_floor():
